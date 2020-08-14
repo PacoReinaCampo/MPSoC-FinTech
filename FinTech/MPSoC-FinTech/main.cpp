fa75cdcebd83b44aa0d7e96779904e8be104324f
PacoReinaCampo <frareicam@gmail.com>
2020-08-14 04:59:34 +0200

    new commit

--- FinTech/MPSoC-FinTech/main.cpp
+++ FinTech/MPSoC-FinTech/main.cpp
@@ -19,7 +19,6 @@
 #include <chrono>
 #include <stdint.h>
 #include <limits.h>
-#include <statistics.h>
 #include <regex>
 
 // Systems Complementaries
@@ -31,6 +30,7 @@
 // OpenNN includes
 
 #include "../../mpsoc-ntm/opennn/opennn.h"
+#include "../../mpsoc-ntm/opennn/statistics.h"
 
 using namespace OpenNN;
 using namespace std;
