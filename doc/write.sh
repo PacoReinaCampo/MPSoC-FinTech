99aebfa44e5e9c5636d9cd058082dcefcef39d63
PacoReinaCampo <frareicam@gmail.com>
2022-08-08 11:48:57 +0200

    Working in MPSoC-FinTech 🦀

--- doc/write.sh
+++ doc/write.sh
@@ -1,5 +1,5 @@
 rm -f *.pdf
 rm -f *.tex
 
-pandoc ../BOOK.md -s -o MPSoC-FinTech.pdf
-pandoc ../BOOK.md -s -o MPSoC-FinTech.tex
+pandoc BOOK.md -s -o MPSoC-FinTech.pdf
+pandoc BOOK.md -s -o MPSoC-FinTech.tex
